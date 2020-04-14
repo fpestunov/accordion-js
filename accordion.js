8b1f8aab58a3d2a1ce94cb56a6159436a4bbad02
fpestunov <fpestunov@gmail.com>
2020-04-14 21:08:32 +0400

    add javascript

---
+++ accordion.js
@@ -0,0 +1,12 @@
+const acc = document.getElementsByClassName("accordion");
+
+for (let i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    const panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
